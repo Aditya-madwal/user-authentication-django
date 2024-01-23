04bcf96db8ff9a266098bf25fb375701a189ee37
Aditya-madwal <madwaladitya@gmail.com>
2024-01-23 14:48:22 +0530

    removed unwanted code components

--- login_signup/forms.py
+++ login_signup/forms.py
@@ -5,18 +5,6 @@ from django.forms.widgets import TextInput, PasswordInput
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class register_form(forms.ModelForm):
-    class Meta:
-        model = register
-        fields = "__all__"
-        widgets = {
-            "username":  TextInput(attrs={'placeholder':'ex:test','autocomplete': 'off'}), 
-            "password": PasswordInput(attrs={'placeholder':'********','autocomplete': 'off','data-toggle': 'password'}),
-        }
-        labels = {
-            "username": None,
-            "password": None
-        }
 
 class user_registeration_form(UserCreationForm) :
     # now this user_registeration_form is just a replica of the real usercreationform since we called it as arg in the brackets
--- login_signup/models.py
+++ login_signup/models.py
@@ -4,14 +4,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class register(models.Model) :
-    username = models.CharField(max_length=50, unique = True)
-    name = models.CharField(max_length = 100,blank=True, null=True)
-    password = models.CharField(max_length=12)
-
-    def __str__(self):
-        return self.username
-
 class task(models.Model) :
     title = models.CharField(max_length=50)
     desc = models.CharField(max_length=50)
